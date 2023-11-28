64cdf4e53cf20f95efa828c513c9b025f62d1ab5
Alessandro Paucar <apaucare27@gmail.com>
2023-11-28 01:47:24 +0100

    Solved commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Dogs perro1 = new Dogs();
+    perro1.setCode("007");
+    perro1.setName("Boo");
+    perro1.setBirthDate("11/10/19");
+    perro1.setNature(true);
+
+    Person persona1 = new Person();
+    persona1.setId("7000000F");
+    persona1.setName("Alessandro");
+    persona1.setSurname("Paucar");
+    persona1.setPhone("600 000 000");
+
+    Adoption adopcion1 = new Adoption();
+    adopcion1.setCode("001.B.A");
+    adopcion1.setAdoptDate("27/11/23");
+    adopcion1.setAnimals(perro1);
+    adopcion1.setPerson(persona1);
+
+    System.out.println("La adopción " + adopcion1.getCode() + " se ha realizado el " + adopcion1.getAdoptDate());
+    System.out.println("La perra adoptada es " + adopcion1.getAnimals().getName());
+    System.out.println("La persona que ha adoptado es " + adopcion1.getPerson().getName() + " " + adopcion1.getPerson().getSurname());
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+    private String id;
+    private String name;
+    private String surname;
+    private String phone;
+    private Adoption adoption;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Adoption getAdoption() {
+        return adoption;
+    }
+
+    public void setAdoption(Adoption adoption) {
+        this.adoption = adoption;
+    }
+}
